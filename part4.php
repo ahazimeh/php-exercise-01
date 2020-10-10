437cfa140c8185cb4af0b259042e1a5ac7251cbc
ahazimeh <hazimeh95@gmail.com>
2020-10-10 19:52:09 +0300

    part 4

--- part4.php
+++ part4.php
@@ -1,5 +1,17 @@
 <?php
-// $a = Array ( [musicals] => Array ( [0] => Oklahoma [1] => The Music Man [2] => South Pacific ) [dramas] => Array ( [0] => Lawrence of Arabia [1] => To Kill a Mockingbird [2] => Casablanca ) [mysteries] => Array ( [0] => The Maltese Falcon [1] => Rear Window [2] => North by Northwest ) );
-$array= Array ( [musicals] => Array ( [0] => Oklahoma,[1] => The\ Music\ Man,[2]=>South\ Pacific),[dramas] => Array( [0] => Lawrence\ of\ Arabia,[1] => To\ Kill\ a\ Mockingbird,[2] => Casablanca ),[mysteries] => Array ( [0] => The\ Maltese\ Falcon, [1] => Rear\ Window, [2] => North\ by\ Northwest));
-
+$array = Array ( 
+    musical => Array ( 0 => "Oklahoma" , 1 => "The Music Man",2 => "South Pacific" ) ,
+    dramas => Array ( 0 => "Lawrence of Arabia" , 1 => "To Kill a Mockingbird",2 => "Casablanca" ) ,
+    mysteries => Array ( 0 => "The Maltese Falcon" , 1 => "Rear Window",2 => "North by Northwest" ) );
+    $keys = array_keys($array);
+    $i=0;
+    foreach($array as $a){
+        echo strtoupper($keys[$i])."<br>";
+        $j =0;
+        foreach ($a as $a1){
+            echo "----> $j = $a1<br>";
+            $j++;
+        }
+    $i++;
+    }
 ?>
